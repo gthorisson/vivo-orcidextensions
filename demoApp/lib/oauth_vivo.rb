ae6b75ae42107abaa2e0b5b8bd233f20b2359bba
Gudmundur A. Thorisson <gthorisson@gmail.com>
2011-10-16 19:17:56 +0000

    Big step forward: managed to parse VIVO profile as N-Triples, query with graph pattern to pull out info and turn into a hash. This hash OmniAuth can work with in the external-authn proces.

--- demoApp/lib/oauth_vivo.rb
+++ demoApp/lib/oauth_vivo.rb
@@ -1,5 +1,11 @@
 require 'omniauth/oauth'
-require 'multi_json'
+require 'restclient'
+require 'rdf'
+require 'rdf/raptor'
+require 'rdf/ntriples'
+require 'pp'
+include RDF # Bring in several standard vocabularies like FOAF
+
 
 module OmniAuth
   module Strategies
@@ -34,23 +40,65 @@ module OmniAuth
       # Retrieve user profile info from the provider, via the OAuth::Access object
       def user_hash(access_token)
 
-        puts "in user_hash(), fetching profile data via OAuth token"
+        puts "in user_hash(), fetching profile data from VIVO LD store"
+        profile_uri = "/Users/mummi/cvswork/vivo-orcidextensions/demoApp/n80.nt"
+        profilegraph = RDF::Graph.load(profile_uri)
 
-        begin
           #response = access_token.get('/rails/api/users/profile')
-          response = access_token.request(:get,
-                                          'http://vivo.crossref.org/individual/n  80/n80.rdf',
-                                          { 'Accept'=>'application/rdf+xml'})   
-          p "response.body="
-          pp response.body
-        rescue
-          puts "An error occurred when retrieving user profile: #{$!}"
+          #response = access_token.request(:get,
+          #                                'http://vivo.crossref.org/individual/n80',
+          #                                { 'Accept'=>'application/rdf+xml'})
+          #response = RestClient.get 'http://vivo.crossref.org/individual/n80', {:accept => 'text/turtle'}
+          #p "response.body="
+          #pp response.body
+          #stringio = StringIO.new response.body
+          #RDF::Reader.for(:turtle).new(stringio) do |reader|
+          #  reader.each_statement do |statement|
+          #    puts 'stmnt: ' + statement.inspect
+          #  end
+          #end
+
+          #profilegraph = RDF::Graph.load("http://vivo.crossref.org/individual/n80/n80.rdf")
+          # ATTN HARDCODED to static N-Triples file for now, until I sort out crazy Raptor RDF-parser bug on OS X
+          #profilegraph = RDF::Graph.load("/Users/mummi/cvswork/vivo-orcidextensions/demoApp/n80.nt")
+
+        # Check what we've pulled back from VIVO
+        puts "Got RDF response, raw triple assertions="
+        profilegraph.each_statement do |statement|
+          puts 'raw stmt = ' +  statement.inspect
         end
         
-        # ToDo: RDF-to-hash conversion
- 
-        hash = MultiJson.decode(response.body)        
-        return hash['user']
+        # A simple graph pattern to pull out basic profile info
+        query = RDF::Query.new({
+          :person => {
+            RDF.type  => FOAF.Person,
+            RDFS.label => :label,
+            FOAF.firstName => :firstName,
+            FOAF.lastName => :lastName,
+          }
+        })
+#        puts "printing out solutions to query"
+#        query.execute(profilegraph).each do |solution|
+#          print "solution: "
+#          solution.each_binding do |n,v|
+#            puts " #{n}=#{v}"
+#          end
+#          print "\n"
+#        end
+        solution = query.execute(profilegraph).first
+
+        user_hash = solution.to_hash
+        pp user_hash
+        user_hash.stringify_keys! # Change hash keys from symbols to strings
+        pp user_hash
+        user_hash.each do |key, val|
+          puts "checking if val for key #{key} is a URI or a literal: #{val.inspect} "
+          user_hash[key] = val.to_s if val.respond_to? :to_s
+        end
+        pp user_hash
+        user_hash['id']  = profile_uri
+        user_hash['uri'] = profile_uri
+        return user_hash
       end
 
       # Do later: handle 301 redirects to get to VIVO profile properly
