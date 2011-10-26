066a38a3bd19ae41b07bb30639d64453ee3e68ba
Gudmundur A. Thorisson <gthorisson@gmail.com>
2011-10-26 17:24:24 +0000

    Finally figured out how to get from the user account object to the profile URI. A non-trivial piece of JRuby/Java maneuvering.

--- productMods/WEB-INF/railsext/app/controllers/application_controller.rb
+++ productMods/WEB-INF/railsext/app/controllers/application_controller.rb
@@ -25,6 +25,11 @@ class ApplicationController < ActionController::Base
     puts "in custom authn routine for VIVO user"
     puts "Rails params from authz form:"
     pp params
+
+    puts "Session environment values:"
+    env.each do |k,v|
+      puts "   #{k} => #{v}"
+    end
     
     puts "servlet request object:"
     pp servlet_request
@@ -43,27 +48,43 @@ class ApplicationController < ActionController::Base
     }
 
     puts "Full request URL=" + servlet_request.request_uri + (servlet_request.query_string|| "")
+
+    # Get handle into the Jena triplestore
+    daoFactory = env['fullWebappDaoFactory']
+    individualDao = daoFactory.getIndividualDao()
     
     # Pull some information out of the Java servlet session
-    session = servlet_request.session()    
+    session     = servlet_request.session()    
     loginStatus = session.get_attribute("loginStatus")
+    selfEdConf  = session.get_attribute("edu.cornell.mannlib.vitro.webapp.beans.SelfEditingConfiguration")
     if loginStatus
       puts "Got loginStatus.toString=" + loginStatus.to_string
+      puts "Got selfEdConf.toString=" + selfEdConf.to_string
       
-      # Instantiate the user in Rails, may need to create one if it doesn't exist
-      #vivo_user = loginStatus.getCurrentUser(session)
-      puts "tricky, calling getCurrentUser method with session argument:"
+      # Instantiate the user in Rails, may need to create one if it doesn't exist. This is non-trivial
+      # due to overloaded methods in Java which need special handling on the JRuby side
+      puts "very tricky, calling getCurrentUser method with session argument:"
       vivo_user = loginStatus.java_send :getCurrentUser, [java.lang.Class.for_name("javax.servlet.http.HttpSession")], session
-      #vivo_user = loginStatus.java_send :getCurrentUser, [org.apache.catalina.session.StandardSessionFacade], session
-      #vivo_user = loginStatus.java_send :getCurrentUser, [javax.servlet.http.HttpServletRequest], servlet_request
-      #method = loginStatus.java_method :getCurrentUser, [javax.servlet.http.HttpServletRequest]
+      puts "Got vivo_user.toString: " + vivo_user.to_string
 
-      puts "Got vivo_user.toString" + vivo_user.to_string
+
+      # Find the profile URI associated with this account, if there is one
+      indList = selfEdConf.getAssociatedIndividuals(individualDao, vivo_user)
+      puts 'list of individuals associated with account: ' + indList.to_string
+      profile_uri = nil
+      if indList.size > 0
+        ind = indList.iterator.next()        
+        profile_uri = ind.get_uri()
+        puts "FINALLY got profile URI = #{profile_uri}"
+      end
+
+      
+
       puts "creating-or-finding Rails user w/ email=" + vivo_user.email_address
       @current_user = User.find_by_email(vivo_user.email_address)
       if !@current_user
-        puts "User does not exist in Rails, need to create one with email="+vivo_user.email_address+", uri="+vivo_user.uri
-        @current_user = User.new(:email => vivo_user.email_address, :uri => vivo_user.uri)
+        puts "User does not exist in Rails, need to create one with email="+vivo_user.email_address+", uri="+profile_uri
+        @current_user = User.new(:email => vivo_user.email_address, :uri => profile_uri)
         @current_user.save!
       end
     else
